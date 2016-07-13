d70cab2b153d156ca230c224a0572c71270cc1b7
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-13 18:10:09 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,3 @@
-// Used to toggle the menu on smaller screens when clicking on the menu button
-function openNav() {
-    var x = document.getElementById("navS");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
-
-$(document).ready(function() {
-
-    // $('nav').load('../assets/common/nav.html');
-    // $('#navS').load('../assets/common/navS.html');
-
-    // $('footer').load('../assets/common/footer.html');
-});
-
 function show_login_pop(){
     document.getElementById("pop-wrap").style.display = "block";
 }
