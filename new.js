cd1f8aeecd1846d9079702abb6eba6e5316cdd73
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-19 00:10:47 -0400

    new.js

--- new.js
+++ new.js
@@ -53,7 +53,7 @@ router.post('/signup', function(req, res){
              } else {
                 if (JSON.stringify(result.rows) === "[]") {
                     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-                        client.query('INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', ' + 'null' + ', ' + 'null, ' + 'null, ' + 'null, ' + ' null, ' + ' toronto, ' + 'canada, ' + 'null, ' + 'null, ' + 'null' + ');', function(err, result){
+                        client.query('INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', ' + 'null' + ', ' + 'null, ' + 'null, ' + 'null, ' + ' null, ' + "'" + 'toronto', "'" + 'canada, ' + 'null, ' + 'null, ' + 'null' + ');', function(err, result){
                             done();
                             if (err) {
                                 console.error(err); 
