92001fbf1f42281197e1542066b144cef73c8602
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-19 00:02:02 -0400

    new.js

--- new.js
+++ new.js
@@ -52,7 +52,10 @@ router.post('/signup', function(req, res){
                  res.send("Error " + err); 
              } else {
                 if (JSON.stringify(result.rows) === "[]") {
-                    //client.query('INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', "null", "null", "null", "null", "null", "toronto", "canada", "null", "null", "null");');
+                    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+                        client.query('INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', "null", "null", "null", "null", "null", "toronto", "canada", "null", "null", "null");');
+                        done();
+                    });
     
                     
                  } else {
