dbf2407d904faa856334f40f80d7a39b053531e8
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-18 20:18:23 -0400

    changed singup

--- new.js
+++ new.js
@@ -34,17 +34,19 @@ router.post('/account', function(req, res){
 
 router.post('/signup', function(req, res){
     
-    //var result = res;
+    var final;
     var account = req.body.email;
     //console.log(account);
 
     var password = req.body.password;
+
     //console.log(password);
     //var query = 'SELECT * FROM user_account WHERE user_account.email = ' + "'" + account + "'" +  ' AND user_account.password =' + "'" + password + "'";
 
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM user_account WHERE user_account.email = ' + "'" + account + "'" +  ' AND user_account.password =' + "'" + password + "'" , function(err, result) {
               done();
+              final = result;
               console.log("hahahahahahahahaha fuck youuuuuuu  111111           "+ JSON.stringify(result.rows));
               if (err) {
                  console.error(err); 
@@ -52,7 +54,7 @@ router.post('/signup', function(req, res){
              }
              else {
                   
-                 if (result.length === 0) {
+                 if (final.length === 0) {
                     //query = 'INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', "null", "null", "null", "null", "null", "toronto", "canada", "null", "null", "null");';
                     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
                         client.query('INSERT INTO user_account (username, email, password, first_name, last_name, profile_pic, gender, phone_num, city, country, date_of_birth, date_joined, description) VALUES (' + "'"+ account + "'" + ", '" + account + "'" + ", '" + password + "'" +', "null", "null", "null", "null", "null", "toronto", "canada", "null", "null", "null");', function(err, result) {
