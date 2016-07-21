008620804c9743c3953e26cdd34d36405acb5ca5
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-21 18:12:52 -0400

    try to create base model

--- test/pg.js
+++ test/pg.js
@@ -1,12 +1,11 @@
 var expect = require('chai').expect;
 
-describe("PostgreSQL", function() {
-    it("is there a server running", function(done) {
+describe('PostgreSQL', function() {
+    it('should run a db server', function(done) {
         var pg = require('pg');
-        pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
             expect(err).to.equal(null);
             expect(client).to.not.be.undefined;
-            done();
         });
         done();
     });
