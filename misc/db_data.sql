7be4ed790965a44e632ea1ddec50d42fa49cc439
Barry Yung <barry.yung@mail.utoronto.ca>
2016-07-22 18:50:01 -0400

    added the product_post data

--- misc/db_data.sql
+++ misc/db_data.sql
@@ -1,63 +1,71 @@
-INSERT INTO city VALUES(
-    select country_id from country where country='Canada', 'Toronto', 'Best City in the world');
-INSERT INTO city VALUES(
-    select counrty_id from counrty where country='Jamaica', 'Kingston', 'Best City in the world');
-INSERT INTO city VALUES(
-    select country_id from counrty where counrty='Japan', 'Tokyo', 'Best City in the world');
-INSERT INTO city VALUES(
-    select county_id from country where country='Brazil', 'Sao Paulo',  'Best City in the world');
-INSERT INTO city VALUES(
-    select country, 'Manila', 'Best City in the world');
-INSERT INTO city VALUES(1, 'New York', 'Best City in the world');
-INSERT INTO city VALUES(default, 'Osaka',  'Best City in the world');
-INSERT INTO city VALUES(1, 'Mumbai', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Delhi', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Lagos', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Cairo', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Paris', 'Best City in the world');
-INSERT INTO city VALUES(1, 'London', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Beijing', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Karachi', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Moscow', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Tehran', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Lima', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Seoul', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Delhi', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Los Angeles', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Chicago', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Miami', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Madrid', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Singapore', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Detroit', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Washington', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Kolkata', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Shanghai', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Essen', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Bogota', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Chennai', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Lahore', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Philadelphia', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Houston', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Sydney', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Berlin', 'Best City in the world');
-INSERT INTO city VALUES(1, 'San Franciso', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Melbourne', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Cape Town', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Rome', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Ankara', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Durban', 'Best City in the world');
-INSERT INTO city VALUES(1, 'Porto Alegre', 'Best City in the world');
+INSERT INTO city VALUES(default, 1, 'New York', 'Best City in the world');
+INSERT INTO city VALUES(default, 2, 'Osaka',  'Best City in the world');
+INSERT INTO city VALUES(default, 3, 'Mumbai', 'Best City in the world');
+INSERT INTO city VALUES(default, 4, 'Delhi', 'Best City in the world');
+INSERT INTO city VALUES(default, 1, 'Lagos', 'Best City in the world');
+INSERT INTO city VALUES(default, 2, 'Cairo', 'Best City in the world');
+INSERT INTO city VALUES(default, 3, 'Paris', 'Best City in the world');
+INSERT INTO city VALUES(default, 10, 'London', 'Best City in the world');
+INSERT INTO city VALUES(default, 2, 'Beijing', 'Best City in the world');
+INSERT INTO city VALUES(default, 32, 'Karachi', 'Best City in the world');
+INSERT INTO city VALUES(default, 33, 'Moscow', 'Best City in the world');
+INSERT INTO city VALUES(default, 23, 'Tehran', 'Best City in the world');
+INSERT INTO city VALUES(default, 24, 'Lima', 'Best City in the world');
+INSERT INTO city VALUES(default, 15, 'Seoul', 'Best City in the world');
+INSERT INTO city VALUES(default, 13, 'Delhi', 'Best City in the world');
+INSERT INTO city VALUES(default, 50, 'Los Angeles', 'Best City in the world');
+INSERT INTO city VALUES(default, 100, 'Chicago', 'Best City in the world');
+INSERT INTO city VALUES(default, 123, 'Miami', 'Best City in the world');
+INSERT INTO city VALUES(default, 89, 'Madrid', 'Best City in the world');
+INSERT INTO city VALUES(default, 78, 'Singapore', 'Best City in the world');
+INSERT INTO city VALUES(default, 9, 'Detroit', 'Best City in the world');
+INSERT INTO city VALUES(default, 34, 'Washington', 'Best City in the world');
+INSERT INTO city VALUES(default, 67, 'Kolkata', 'Best City in the world');
+INSERT INTO city VALUES(default, 56, 'Shanghai', 'Best City in the world');
+INSERT INTO city VALUES(default, 34, 'Essen', 'Best City in the world');
+INSERT INTO city VALUES(default, 54, 'Bogota', 'Best City in the world');
+INSERT INTO city VALUES(default, 69, 'Chennai', 'Best City in the world');
+INSERT INTO city VALUES(default, 25, 'Lahore', 'Best City in the world');
+INSERT INTO city VALUES(default, 94, 'Philadelphia', 'Best City in the world');
+INSERT INTO city VALUES(default, 66, 'Houston', 'Best City in the world');
+INSERT INTO city VALUES(default, 47, 'Sydney', 'Best City in the world');
+INSERT INTO city VALUES(default, 45, 'Berlin', 'Best City in the world');
+INSERT INTO city VALUES(default, 88, 'San Franciso', 'Best City in the world');
+INSERT INTO city VALUES(default, 111, 'Melbourne', 'Best City in the world');
+INSERT INTO city VALUES(default, 124, 'Cape Town', 'Best City in the world');
+INSERT INTO city VALUES(default, 49, 'Rome', 'Best City in the world');
+INSERT INTO city VALUES(default, 72, 'Ankara', 'Best City in the world');
+INSERT INTO city VALUES(default, 75, 'Durban', 'Best City in the world');
+INSERT INTO city VALUES(default, 90, 'Porto Alegre', 'Best City in the world');
 
-INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
-        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO user_account VALUES(default, 'Bob123', 'bob@email.com', 'abasg23456', 'Bob', 'Martin',
+        'm', 123456789, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
 
-
-INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
-        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
-
-
-INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
-        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
-
-INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
-        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+INSERT INTO product_post VALUES(default, 1, 'guide', '2014-04-01', 'by plane', '2014-10-02', '2014-10-14');
+INSERT INTO product_post VALUES(default, 2, 'guide', '2014-05-02', 'by train', '2014-11-11', '2014-11-04');
+INSERT INTO product_post VALUES(default, 3, 'guide', '2014-05-03', 'by plane', '2014-12-01', '2015-01-01');
+INSERT INTO product_post VALUES(default, 4, 'buddy', '2014-06-04', 'by plane', '2015-01-01', '2014-01-23');
+INSERT INTO product_post VALUES(default, 5, 'guide', '2014-07-05', 'by foot', '2015-02-23', '2014-04-05');
+INSERT INTO product_post VALUES(default, 6, 'buddy', '2014-08-06', 'by ferry', '2015-05-01', '2014-06-21');
+INSERT INTO product_post VALUES(default, 7, 'guide', '2014-09-07', 'by ferry', '2015-06-03', '2014-08-17');
+INSERT INTO product_post VALUES(default, 8, 'buddy', '2015-04-08', 'by plane', '2015-08-30', '2015-09-01');
+INSERT INTO product_post VALUES(default, 9, 'guide', '2015-05-09', 'by train', '2015-09-01', '2015-09-18');
+INSERT INTO product_post VALUES(default, 10, 'guide', '2015-11-10', 'by plane', '2015-12-25', '2016-01-13');
