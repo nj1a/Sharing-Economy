a8dcb8c1ff2b90f739df91012d3b30c98106e918
Yik Chan <Navie@MacBook-Pro-2.local>
2016-07-22 01:37:13 -0400

    work on post page

--- new.js
+++ new.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var tool = require('./assets/js/db_function.js');
+var tool = require('public/assets/js/db_function.js');
 
 router.get('/', function(req, res) {
     res.render('index');
