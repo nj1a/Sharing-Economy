463b8af6c24a0c88a1e6f33b15f5f52ec6350d48
Barry Yung <barry.yung@mail.utoronto.ca>
2016-07-22 18:20:53 -0400

    added data db_data.sql

--- misc/db_data.sql
+++ misc/db_data.sql
@@ -1,9 +1,74 @@
-INSERT INTO country (name) VALUES('Canada');
+INSERT INTO city VALUES(
+    select country_id from country where country='Canada', 'Toronto', 'Best City in the world');
+INSERT INTO city VALUES(
+    select counrty_id from counrty where country='Jamaica', 'Kingston', 'Best City in the world');
+INSERT INTO city VALUES(
+    select country_id from counrty where counrty='Japan', 'Tokyo', 'Best City in the world');
+INSERT INTO city VALUES(
+    select county_id from country where country='Brazil', 'Sao Paulo',  'Best City in the world');
+INSERT INTO city VALUES(
+    select country, 'Manila', 'Best City in the world');
+INSERT INTO city VALUES(1, 'New York', 'Best City in the world');
+INSERT INTO city VALUES(default, 'Osaka',  'Best City in the world');
+INSERT INTO city VALUES(1, 'Mumbai', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Delhi', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Lagos', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Cairo', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Paris', 'Best City in the world');
+INSERT INTO city VALUES(1, 'London', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Beijing', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Karachi', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Moscow', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Tehran', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Lima', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Seoul', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Delhi', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Los Angeles', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Chicago', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Miami', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Madrid', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Singapore', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Detroit', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Washington', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Kolkata', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Shanghai', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Essen', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Bogota', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Chennai', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Lahore', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Philadelphia', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Houston', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Sydney', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Berlin', 'Best City in the world');
+INSERT INTO city VALUES(1, 'San Franciso', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Melbourne', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Cape Town', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Rome', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Ankara', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Durban', 'Best City in the world');
+INSERT INTO city VALUES(1, 'Porto Alegre', 'Best City in the world');
 
-INSERT INTO city (country_id, name, continent, description) VALUES(1, 'Toronto', 'North America', 'Best City in the world');
 
-INSERT INTO user_account (username, email, password, first_name, last_name,
-    profile_pic, gender, phone_num, city_id, country_id, date_of_birth,
-    date_joined, description, is_admin)
-VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
+
+
+
+
+
+
+
+
+
+
+INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
+        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+
+
+INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
+        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+
+
+INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
+        './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
+
+INSERT INTO user_account VALUES('Bob123', 'bob@email.com', 'abcdef123456', 'Bob', 'Martin',
         './pic.png', 'm', 1234567890, 1, 1, '12/12/1980', '12/12/2014', 'Hi, I am Bob', false);
