991dec18e114cb90156ab7be857c66a80f05db93
Yik Chan <Navie@MacBook-Pro-2.local>
2016-07-22 01:48:01 -0400

    work on post page

--- new.js
+++ new.js
@@ -108,9 +108,9 @@ router.post('/signup', function(req, res){
 });
 // Post page
 router.get('/post/:postId', function(req, res){
-    // var country = tool.tmp;
-    // res.send(country);
-    res.send('This is post page with id is '+req.params.postId);
+    var country = tool.tmp;
+    res.send(country);
+    // res.send('This is post page with id is '+req.params.postId);
 });
 
 module.exports = router;
