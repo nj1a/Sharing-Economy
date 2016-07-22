eb569c119371c1af83c3c13caca186b385e25470
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-22 15:28:37 -0400

    adding new files

--- public/index_script.js
+++ public/index_script.js
@@ -3,20 +3,34 @@ window.slideNum = 1;
 $(document).ready(function() {
 
 	startSlide();
+	var clicked=false;
 
-	$("input[type='submit']#existingClient").click(function(e){
-				
+	var email,pass;
+   
+
+
+
+	$("input[type='button']#existingClient").click(function(e){
+		
+		clicked=true;
+			
+			
 		return validateEmailandPassword("existing");
+		
+		
 	});
+
+	
 
 	$("input[type='submit']#newClient").click(function(e){
 
 		return validateEmailandPassword("new");
 	});
 
+
+
 	$(document.getElementById("root-content")).click(function(e){
-		//alert(e.target.class);
-	
+		
 		if (e.target.className != "existing" &&
 			e.target.className != "inputField" &&
 			e.target.className != "hover underlined" &&
@@ -37,8 +51,21 @@ $(document).ready(function() {
 
 	});
 
+	function checkError(arg){
+		if (arg > 0) {
+			return false;
+		}
+		return true;
+	}
+
+	
+
+
+
+
 	function validateEmailandPassword(client) {
 		var isValid = true;
+		
 
 		if ($("span.error").length) $("span.error").remove();
 
@@ -56,51 +83,71 @@ $(document).ready(function() {
 
 		if ( inputEmail == "" && client =="existing") {
 			$('input#emailEx').after("<span class='error'>Provide a username.<br></span>");
-			isValid = false;
+			return false;
 
 		} else if (inputEmail == "" && client =="new") {
 			$('input#emailNew').after("<span class='error'>Provide a username.<br></span>");
-			isValid = false;
+			return false;
 
 		} else if ( is_email(inputEmail) != true && client =="existing") {
 			
 			$('input#emailEx').after("<span class='error'>Invalid format<br></span>");
-			isValid = false;
+			return false;
 
 		} else if (is_email(inputEmail) != true  && client =="new") {
 			$('input#emailNew').after("<span class='error'>Invalid format<br></span>");
-			isValid = false;
+			return false;
 		} 
 
 		if ( inputPassword == "" && client =="existing"){
 			$("input#passwordEx").after("<span class='error'>Provide a password.<br></span>");
-			isValid = false;
+			return false;
 
 		} else if (inputPassword == "" && client =="new") {
 			$("section#passnew").after("<span class='error span'>Provide a password.<br></span>");
-			isValid = false;
+			return false;
 
 		} else if ( inputPassword.length < 8 && client == "existing") {
 			$("input#passwordEx").after("<span class='error'>Password must be at least 8 characters<br></span>");
-			isValid = false;
+			return false;
 
 		} else if (inputPassword.length < 8 && client =="new"){
 			$("section#passnew").after("<span class='error span'>Password must be at least 8 characters<br></span>");
-			isValid = false;
+			return false;
 		}
 
 		if (client == "new") {
 			if (inputPassword.length >= 8 && (inputPassword != inputPasswordConfirm)) {
 				$("section#passnew").after("<span class='error span'>Password must match<br></span>");
-				isValid = false;
+				return false;
 
 			}
 
 		}
 
+		email=$("#emailEx").val();
+        pass=$("#passwordEx").val();
+
+        //alert(email);
+        //alert(pass);
+		$.post("/login",{email:email,pass:pass},function(data){        
+            if(data==='done')           
+            {	
+            	//alert(1);
+                window.location.href="/profile";
+            } else {
+            	//alert(2);
+            	show_login_pop();
+            	isValid = false;
+
+            }
+    	});
+
+
 		return isValid;
 	
 	}
+	
 
 	function is_email(email) {
 		
@@ -118,6 +165,10 @@ $(document).ready(function() {
 	}
 	
 });
+
+
+
+
 
 
 function show_login_pop(){
@@ -140,7 +191,7 @@ function startSlide(){
 	var i;
 	slideNum += 1;
 	var images = document.getElementsByClassName("background-image");
-	//alert(slideNum);
+	
 	if (slideNum > images.length) {
 		slideNum = 1;
 	} 
