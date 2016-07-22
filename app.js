bacaa406cb6510be734901709385a82765aae34e
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-22 15:01:40 -0400

    updating app, new.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
+var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
+var pg = require('pg');
 
 var app = express();
 
@@ -13,6 +15,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+
 
 // define routes
 var router = require('./new');
