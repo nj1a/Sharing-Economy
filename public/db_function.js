149c2e32f4ceb182a9cf9b8a3ca66bceae4a16c7
Yik Chan <Navie@MacBook-Pro-2.local>
2016-07-23 01:52:28 -0400

    db_function

--- public/db_function.js
+++ public/db_function.js
@@ -3,7 +3,8 @@ var client =
 module.exports = {
 	get_info_by_post_id : function(post_id, callback){
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			client.query('SELECT * FROM product_post, user_account WHERE post_id ='+post_id+' AND product_post.user_id = user_account.user_id', function(err, result){
+			client.query('SELECT * FROM product_post, user_account, city AS ci1, city AS ci2, country AS co1, country AS co2 WHERE post_id ='+post_id+' AND product_post.user_id = user_account.user_id AND from_city = ci1.city_id AND to_city = ci2.city
+				 AND ci1.country_id = co1.country_id AND ci2.country_id = ci2.country_id', function(err, result){
 				done();
 				if (err) throw err;
 				else{
@@ -11,7 +12,7 @@ module.exports = {
 						console.log('No matching row in database');
 						callback('error');
 					} else {
-						// console.log(result.rows[0]);
+						console.log(result.rows[0]);
 						callback(result.rows[0]);
 					}
 				}
