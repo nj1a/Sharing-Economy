db17f7e9207e3edabe4aa91b6b3794adb4f534b2
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-24 01:32:34 -0400

    basic chat

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var Room = require('./room');  
+var uuid = require('node-uuid');
 
-var app = express();
+
+
+
 
 var Admin = require('./controllers/admin');
 // configure environemnt
@@ -16,6 +23,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
+app.get('/chat', function(req, res){
+  res.sendfile('./public/chat.html');
+});
+
+io.on('connection', function(socket){
+    
+    socket.on('little_newbie', function(username) {
+        socket.username = username;
+        console.log('%s connected', username);
+    });
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log(socket.username + ' is speaking to me! They\'re saying: ' + msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 
 // define routes
 
@@ -35,12 +61,17 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 
         // start the server
         var port = process.env.PORT || 1337;
-        app.listen(port, function () {
+        server.listen(port, function () {
             console.log('listen on port ' + port);
         });
   // }
 });
 
+
+io.set("log level", 1); 
+var people = {};  
+var rooms = {};  
+var clients = []; 
 
 var router = require('./new');
 app.use(router);
