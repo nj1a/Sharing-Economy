47800a240cd945d99947412a30ea5efad8cabc16
Barry Yung <barry.yung@mail.utoronto.ca>
2016-07-25 20:49:09 -0400

    mod

--- new.js
+++ new.js
@@ -179,8 +179,7 @@ router.post('/result', parseForm, csrfProtection, function(req, res) {
                             // res.send(JSON.stringify(result));
                             console.log('This is result object: ', result3);
                             res.render("result", {
-                                result: result3,
-                                csrfToken: req.csrfToken()
+                                result: result3
                             });
                         }
                     });
