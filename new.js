d1f5036b0ced7254599aa62a6ee6df93d6cfa80a
Yik Chan <Navie@MacBook-Pro-2.local>
2016-07-25 20:05:06 -0400

    create post

--- new.js
+++ new.js
@@ -581,7 +581,7 @@ router.get('/post/:postId', csrfProtection, function(req, res){
         if (result === 'error') {
           res.send('No such result in database');
         } else{
-            glob('public/img/post_images'+req.params.postId+'_*.*', function(er, files){
+            glob('public/img/post_images'+req.params.postId+'_*.jpg', function(er, files){
                 if (er) {
                     throw er;
                 }
