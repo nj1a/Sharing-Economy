d0b3961a631554528e6755a79efbc20a28d0f4ea
Sang Jun Ahn <sang.ahn@mail.utoronto.ca>
2016-07-25 20:05:27 -0400

    merging

--- new.js
+++ new.js
@@ -281,7 +281,7 @@ router.get('/profile', function(req, res){
                             }
                             res.render('profile', {
                                 results: result1.rows,
-                                csrfToken: req.csrfToken(),
+                                //csrfToken: req.csrfToken(),
                                 errors: ' ',
                                 type: 'own',
                                 pic: path
