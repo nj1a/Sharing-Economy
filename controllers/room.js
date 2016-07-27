5abc317d7a76bb16568536c4e1cd650cb191905d
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-26 23:16:22 -0400

    message

--- controllers/room.js
+++ controllers/room.js
@@ -1,11 +1,11 @@
 function Room(name, id, owner) {
-  this.name = name;
-  this.id = id;
-  this.owner = owner;
-  this.people = [];
-  this.peopleLimit = 4;
-  this.status = "available";
-  this.private = false;
+    this.name = name;
+    this.id = id;
+    this.owner = owner;
+    this.people = [];
+    this.peopleLimit = 4;
+    this.status = "available";
+    this.private = false;
 }
 
 Room.prototype.addPerson = function(personID) {
