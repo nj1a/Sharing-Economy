79aa38edd8384c8f2255edba52f43136e4d8fa56
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-27 15:14:51 -0400

    try sess

--- public/js/message.js
+++ public/js/message.js
@@ -226,21 +226,4 @@ $(document).ready(() => {
         currRoom = data.id;
     });
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-var constraints = {video: true};
-
-function successCallback(localMediaStream) {  
-  window.stream = localMediaStream;
-  var video = document.querySelector("video");
-  video.src = window.URL.createObjectURL(localMediaStream);
-  video.play();
-}
-
-function errorCallback(error){  
-  console.log("navigator.getUserMedia error: ", error);
-}
-
-navigator.getUserMedia(constraints, successCallback, errorCallback);  
-
 });
