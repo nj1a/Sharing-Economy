c8a3702b3c87f10395067d724769c811a4e68a0c
Yik Chan <naviechan@gmail.com>
2016-07-27 16:04:01 -0400

    world map search

--- new.js
+++ new.js
@@ -214,7 +214,11 @@ router.get('/world_map', csrfProtection, function(req, res){
     res.render('world_map');
 
 });
+// Convert ISO country code to country id
+router.get('/get_country/:countryCode', csrfProtection, function(req, res){
+    res.render('THis is country code: 'countryCode);
 
+});
 router.post('/result', function(req, res) {
     console.log(req.body);
     console.log('hi');
