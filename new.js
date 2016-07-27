4a84e8916e36a8d146e90d4d728c7b4afa9d9c57
Yik Chan <Navie@MacBook-Pro-2.local>
2016-07-27 13:40:42 -0400

    suggested post

--- new.js
+++ new.js
@@ -323,7 +323,7 @@ router.get('/city/:cityID', csrfProtection, function(req, res){
                     // Get ratings and comments
                     tool.get_ratings_by_city_id(req.params.cityID, function(ratings){
                         if (ratings === 'error') {
-                            // res.send('City not found');
+                            res.send('City not found');
                             ratings = {};
                         };
 
