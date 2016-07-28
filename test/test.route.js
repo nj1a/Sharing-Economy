84954fe10d5de1302e00caa71de8ac77a87eb3e0
njia95 <nathan.jia@mail.utoronto.ca>
2016-07-27 21:52:11 -0400

    add tests

--- test/test.route.js
+++ test/test.route.js
@@ -4,7 +4,7 @@ var request = require('superagent');
 
 describe("route", () => {
 	it("should access the homepage", done => {
-		request.get('http://localhost:1337').end((err, res) => {
+		request.get('https://wander-land.herokuapp.com').end((err, res) => {
 			expect(res).to.exist;
 			expect(res.status).to.equal(200);
 			expect(res.error).to.equal(false);
@@ -13,7 +13,7 @@ describe("route", () => {
 	});
 
 	it("should see the login success page", done => {
-		request.get('http://localhost:1337/login_success').end((err, res) => {
+		request.get('https://wander-land.herokuapp.com/login_success').end((err, res) => {
 			expect(res).to.exist;
 			expect(res.status).to.equal(200);
 			expect(res.error).to.equal(false);
@@ -22,7 +22,7 @@ describe("route", () => {
 	});
 
 	it("should see the message page", done => {
-		request.get('http://localhost:1337/message').end((err, res) => {
+		request.get('https://wander-land.herokuapp.com/message').end((err, res) => {
 			expect(res).to.exist;
 			expect(res.status).to.equal(200);
 			expect(res.text).to.equal('You need to sign in first');
@@ -32,7 +32,7 @@ describe("route", () => {
 	});
 
 	it("should see the woulrd map", done => {
-		request.get('http://localhost:1337/world_map').end((err, res) => {
+		request.get('https://wander-land.herokuapp.com/world_map').end((err, res) => {
 			expect(res).to.exist;
 			expect(res.status).to.equal(200);
 			expect(res.error).to.equal(false);
@@ -40,8 +40,8 @@ describe("route", () => {
 		});
 	});
 
-	it("should see the woulrd map", done => {
-		request.get('http://localhost:1337/world_map').end((err, res) => {
+	it("should see the create post page", done => {
+		request.get('https://wander-land.herokuapp.com/create_post').end((err, res) => {
 			expect(res).to.exist;
 			expect(res.status).to.equal(200);
 			expect(res.error).to.equal(false);
@@ -49,4 +49,34 @@ describe("route", () => {
 		});
 	});
 
+	it("should get country by key", done => {
+		// try to get Greenland
+		request.get('https://wander-land.herokuapp.com/get_country?key=GL').end((err, res) => {
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			expect(res.error).to.equal(false);
+			done();
+		});
+	});
+
+	it("should show no matching by asking for a wrong country code", done => {
+		// try to get a random code
+		request.get('https://wander-land.herokuapp.com/get_country?key=GLA').end((err, res) => {
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			expect(res.error).to.equal(false);
+			expect(res.text).to.equal('No matching result');
+			done();
+		});
+	});
+
+	it("should get city by key", done => {
+		// try to get New York
+		request.get('https://wander-land.herokuapp.com/get_city?key=1').end((err, res) => {
+			expect(res).to.exist;
+			expect(res.status).to.equal(200);
+			expect(res.error).to.equal(false);
+			done();
+		});
+	});
 });
