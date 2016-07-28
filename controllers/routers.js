5fb5295b1a2f5c55967d5af44388ae9d92224213
Yik Chan <naviechan@gmail.com>
2016-07-27 23:36:32 -0400

    ha

--- controllers/routers.js
+++ controllers/routers.js
@@ -1130,24 +1130,24 @@ router.post('/create_post', function(req, res){
 
                             }else{
                                 // result3.post_id
-                                // res.redirect('/post/'+result3.post_id);
+                                res.redirect('/post/'+result3.post_id);
 
                                 // res.send(JSON.stringify(result));
-                                console.log('This is result object: ', result3);
-                                // res.send('Your post_id is: '+result3.post_id);
-                                var fstream;
-                                req.pipe(req.busboy);
-                                req.busboy.on('file', function (fieldname, file, filename) {
-                                   // console.log("Uploading: " + filename + "for the user:   " + usrID + "to the folder:  " + __dirname + '/../public/img/' + "profile_" + usrID);
-                                    fstream = fs.createWriteStream('public/img/' + "profile_" + "2");
-                                    file.pipe(fstream);
-                                    fstream.on('close', function () {
+                                // console.log('This is result object: ', result3);
+                                // // res.send('Your post_id is: '+result3.post_id);
+                                // var fstream;
+                                // req.pipe(req.busboy);
+                                // req.busboy.on('file', function (fieldname, file, filename) {
+                                //    // console.log("Uploading: " + filename + "for the user:   " + usrID + "to the folder:  " + __dirname + '/../public/img/' + "profile_" + usrID);
+                                //     fstream = fs.createWriteStream('public/img/' + "profile_" + "2");
+                                //     file.pipe(fstream);
+                                //     fstream.on('close', function () {
 
-                                        res.redirect('/profile');
+                                //         res.redirect('/profile');
 
-                                    });
-                                });
-
+                                //     });
+                                // });
+                            
                             }
                         });
 
